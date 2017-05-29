0878514106bea28431b32ee0720a2587c926dca2
Volker <volker.foth@uni-ulm.de>
2017-05-29 14:01:50 +0200

    intial converters

---
+++ src/main/java/io/github/cloudiator/rest/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package io.github.cloudiator.rest;
+
+/**
+ * Created by volker on 29.05.17.
+ */
+public class UserServiceImpl {
+}
---
+++ src/main/java/io/github/cloudiator/rest/converter/TwoWayConverter.java
@@ -0,0 +1,7 @@
+package io.github.cloudiator.rest.converter;
+
+/**
+ * Created by volker on 29.05.17.
+ */
+public class TwoWayConverter {
+}
