44c1009885832b364caa0eb6e1b823cec4d96484
Volker <volker.foth@uni-ulm.de>
2017-06-27 15:02:48 +0200

    apiexception edited

--- src/main/java/io/github/cloudiator/rest/api/ApiException.java
+++ src/main/java/io/github/cloudiator/rest/api/ApiException.java
@@ -1,13 +1,16 @@
 package io.github.cloudiator.rest.api;
 
 
-public class ApiException extends RuntimeException{
-    private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
-        this.code = code;
-    }
-    public int getCode() {
-        return code;
-    }
+public class ApiException extends RuntimeException {
+
+  private int code;
+
+  public ApiException(int code, String msg) {
+    super(msg);
+    this.code = code;
+  }
+
+  public int getCode() {
+    return code;
+  }
 }
