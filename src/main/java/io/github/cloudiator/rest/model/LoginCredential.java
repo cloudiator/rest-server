c2d7305feec5d775e8356dfce14e27dc879c2bb1
Daniel Seybold <daniel.seybold@uni-ulm.de>
2017-09-12 14:55:18 +0200

    complete node model

--- src/main/java/io/github/cloudiator/rest/model/LoginCredential.java
+++ src/main/java/io/github/cloudiator/rest/model/LoginCredential.java
@@ -1,16 +1,88 @@
 package io.github.cloudiator.rest.model;
 
 import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 /**
- * Platzhalter
+ * temporary definition to create a node
  */
-@ApiModel(description = "Platzhalter")
+@ApiModel(description = "temporary definition to create a node")
 
 public class LoginCredential   {
+  @JsonProperty("user")
+  private String user = null;
+
+  @JsonProperty("password")
+  private String password = null;
+
+  @JsonProperty("key")
+  private String key = null;
+
+  public LoginCredential user(String user) {
+    this.user = user;
+    return this;
+  }
+
+   /**
+   * Get user
+   * @return user
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getUser() {
+    return user;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+  public LoginCredential password(String password) {
+    this.password = password;
+    return this;
+  }
+
+   /**
+   * Get password
+   * @return password
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public LoginCredential key(String key) {
+    this.key = key;
+    return this;
+  }
+
+   /**
+   * Get key
+   * @return key
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
 
   @Override
   public boolean equals(java.lang.Object o) {
@@ -20,13 +92,15 @@ public class LoginCredential   {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    return true;
+    LoginCredential loginCredential = (LoginCredential) o;
+    return Objects.equals(this.user, loginCredential.user) &&
+        Objects.equals(this.password, loginCredential.password) &&
+        Objects.equals(this.key, loginCredential.key);
   }
 
-  
   @Override
   public int hashCode() {
-    return Objects.hash();
+    return Objects.hash(user, password, key);
   }
 
   @Override
@@ -34,6 +108,9 @@ public class LoginCredential   {
     StringBuilder sb = new StringBuilder();
     sb.append("class LoginCredential {\n");
     
+    sb.append("    user: ").append(toIndentedString(user)).append("\n");
+    sb.append("    password: ").append(toIndentedString(password)).append("\n");
+    sb.append("    key: ").append(toIndentedString(key)).append("\n");
     sb.append("}");
     return sb.toString();
   }
