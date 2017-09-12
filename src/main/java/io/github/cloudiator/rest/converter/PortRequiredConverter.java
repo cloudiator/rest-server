c65cf15109901e72d1c5dcbe5820253093bdea07
Volker <volker.foth@uni-ulm.de>
2017-09-12 15:19:26 +0200

    Fixed indentation

--- src/main/java/io/github/cloudiator/rest/converter/PortRequiredConverter.java
+++ src/main/java/io/github/cloudiator/rest/converter/PortRequiredConverter.java
@@ -3,21 +3,24 @@ package io.github.cloudiator.rest.converter;
 import io.github.cloudiator.rest.model.PortRequired;
 import org.cloudiator.messages.entities.TaskEntities;
 
-public class PortRequiredConverter implements TwoWayConverter<PortRequired, TaskEntities.PortRequired> {
-    @Override
-    public PortRequired applyBack(TaskEntities.PortRequired portRequired) {
-        PortRequired result = new PortRequired();
-        result.setName(portRequired.getName());
-        result.setIsMandatory(portRequired.getIsMandatory());
-        result.setUpdateAction(portRequired.getUpdateAction());
-        result.setType(result.getClass().getSimpleName());
-        return result;
-    }
+public class PortRequiredConverter implements
+    TwoWayConverter<PortRequired, TaskEntities.PortRequired> {
 
-    @Override
-    public TaskEntities.PortRequired apply(PortRequired portRequired) {
-        TaskEntities.PortRequired.Builder result = TaskEntities.PortRequired.newBuilder();
-        result.setName(portRequired.getName()).setIsMandatory(portRequired.getIsMandatory()).setUpdateAction(portRequired.getUpdateAction());
-        return result.build();
-    }
+  @Override
+  public PortRequired applyBack(TaskEntities.PortRequired portRequired) {
+    PortRequired result = new PortRequired();
+    result.setName(portRequired.getName());
+    result.setIsMandatory(portRequired.getIsMandatory());
+    result.setUpdateAction(portRequired.getUpdateAction());
+    result.setType(result.getClass().getSimpleName());
+    return result;
+  }
+
+  @Override
+  public TaskEntities.PortRequired apply(PortRequired portRequired) {
+    TaskEntities.PortRequired.Builder result = TaskEntities.PortRequired.newBuilder();
+    result.setName(portRequired.getName()).setIsMandatory(portRequired.getIsMandatory())
+        .setUpdateAction(portRequired.getUpdateAction());
+    return result.build();
+  }
 }
