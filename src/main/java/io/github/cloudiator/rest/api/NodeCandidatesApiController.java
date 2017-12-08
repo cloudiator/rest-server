6bdf0a32865d20412fb701e6b988684aa6f1d6b3
Daniel Baur <daniel.baur@uni-ulm.de>
2017-12-08 13:04:52 +0100

    Avoid null pointer if no requirement is set

--- src/main/java/io/github/cloudiator/rest/api/NodeCandidatesApiController.java
+++ src/main/java/io/github/cloudiator/rest/api/NodeCandidatesApiController.java
@@ -6,8 +6,6 @@ import io.github.cloudiator.rest.converter.NodeCandidateConverter;
 import io.github.cloudiator.rest.converter.NodeRequirementsConverter;
 import io.github.cloudiator.rest.model.NodeCandidate;
 import io.github.cloudiator.rest.model.NodeRequirements;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.cloudiator.messages.entities.Matchmaking.NodeCandidateRequestMessage;
 import org.cloudiator.messages.entities.MatchmakingEntities;
 import org.cloudiator.messaging.ResponseException;
@@ -17,42 +15,50 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class NodeCandidatesApiController implements NodeCandidatesApi {
+import java.util.List;
+import java.util.stream.Collectors;
 
-  private final ObjectMapper objectMapper;
-  private static final NodeCandidateConverter NODE_CANDIDATE_CONVERTER = new NodeCandidateConverter();
-  private static final NodeRequirementsConverter NODE_REQUIREMENTS_CONVERTER = new NodeRequirementsConverter();
+@Controller public class NodeCandidatesApiController implements NodeCandidatesApi {
 
-  @Autowired
-  private UserService userService;
+    private final ObjectMapper objectMapper;
+    private static final NodeCandidateConverter NODE_CANDIDATE_CONVERTER =
+        new NodeCandidateConverter();
+    private static final NodeRequirementsConverter NODE_REQUIREMENTS_CONVERTER =
+        new NodeRequirementsConverter();
 
-  @Autowired
-  private MatchmakingService matchmakingService;
+    @Autowired private UserService userService;
 
-  public NodeCandidatesApiController(ObjectMapper objectMapper) {
-    this.objectMapper = objectMapper;
-  }
+    @Autowired private MatchmakingService matchmakingService;
 
-  @Override
-  public ResponseEntity<List<NodeCandidate>> findNodeCandidates(NodeRequirements nodeRequirements,
-      String accept) throws Exception {
-    if (accept != null && accept.contains("application/json")) {
-
-      try {
-        final List<MatchmakingEntities.NodeCandidate> candidatesList = matchmakingService
-            .requestNodes(
-                NodeCandidateRequestMessage.newBuilder().setUserId(userService.getUserId())
-                    .setRequirements(NODE_REQUIREMENTS_CONVERTER.apply(nodeRequirements)).build())
-            .getCandidatesList();
-
-        return new ResponseEntity<>(
-            candidatesList.stream().map(NODE_CANDIDATE_CONVERTER::applyBack).collect(
-                Collectors.toList()), HttpStatus.OK);
-      } catch (ResponseException e) {
-        return new ResponseEntity<>(HttpStatus.valueOf(e.code()));
-      }
+    public NodeCandidatesApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
-    return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-  }
+
+    @Override
+    public ResponseEntity<List<NodeCandidate>> findNodeCandidates(NodeRequirements nodeRequirements,
+        String accept) throws Exception {
+        if (accept != null && accept.contains("application/json")) {
+
+            try {
+
+                final NodeCandidateRequestMessage.Builder builder =
+                    NodeCandidateRequestMessage.newBuilder().setUserId(userService.getUserId());
+
+                if (nodeRequirements != null && nodeRequirements.getRequirements() != null
+                    && !nodeRequirements.getRequirements().isEmpty()) {
+                    builder.setRequirements(NODE_REQUIREMENTS_CONVERTER.apply(nodeRequirements));
+                }
+
+                final List<MatchmakingEntities.NodeCandidate> candidatesList =
+                    matchmakingService.requestNodes(builder.build()).getCandidatesList();
+
+                return new ResponseEntity<>(
+                    candidatesList.stream().map(NODE_CANDIDATE_CONVERTER::applyBack)
+                        .collect(Collectors.toList()), HttpStatus.OK);
+            } catch (ResponseException e) {
+                return new ResponseEntity<>(HttpStatus.valueOf(e.code()));
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+    }
 }
