a41d90b1a09ff94bf90977f721b554038ea0c158
Daniel Seybold <daniel.seybold@uni-ulm.de>
2018-03-14 18:21:32 +0100

    work on installation request controller

--- src/main/java/io/github/cloudiator/rest/api/InstallerApi.java
+++ src/main/java/io/github/cloudiator/rest/api/InstallerApi.java
@@ -6,17 +6,21 @@
 package io.github.cloudiator.rest.api;
 
 import io.github.cloudiator.rest.model.InstallationRequest;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
-import javax.validation.Valid;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
 
 @Api(value = "installer", description = "the installer API")
 public interface InstallerApi {
