aaa786ac08a618dc12375391a0d7f1a2c9a2678c
Daniel Seybold <daniel.seybold@uni-ulm.de>
2018-03-14 17:25:26 +0100

    changed Install request to POST to avoid CORS issues

--- src/main/java/io/github/cloudiator/rest/api/InstallerApiController.java
+++ src/main/java/io/github/cloudiator/rest/api/InstallerApiController.java
@@ -3,11 +3,12 @@ package io.github.cloudiator.rest.api;
 import io.github.cloudiator.rest.UserService;
 import io.github.cloudiator.rest.converter.InstallationRequestConverter;
 import io.github.cloudiator.rest.model.InstallationRequest;
-import io.github.cloudiator.rest.model.OclSolution;
 import io.swagger.annotations.ApiParam;
 import javax.validation.Valid;
 import org.cloudiator.messages.Installation;
 import org.cloudiator.messaging.services.InstallationRequestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +26,22 @@ public class InstallerApiController implements InstallerApi {
 
   private InstallationRequestConverter installationRequestConverter = new InstallationRequestConverter();
 
-  public ResponseEntity<OclSolution> installTools(
+  private static final Logger LOGGER = LoggerFactory.getLogger(InstallerApiController.class);
+
+  public ResponseEntity<Void> installTools(
       @ApiParam(value = "a request to install the cloudiator tools on a provided node", required = true) @Valid @RequestBody InstallationRequest installationRequest) {
 
+
+    System.out.println("Starting tools installation!");
     //TODO: get installationRequest service
+
+    LOGGER.debug("installing something maybe...");
+
+    try {
+      Thread.sleep(100000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
     installationRequestService.createInstallationRequestAsync(Installation.InstallationRequest.newBuilder()
         .setUserId(userService.getUserId())
@@ -40,7 +53,8 @@ public class InstallerApiController implements InstallerApi {
 
 
 
-    return new ResponseEntity<OclSolution>(HttpStatus.OK);
+
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
 }
