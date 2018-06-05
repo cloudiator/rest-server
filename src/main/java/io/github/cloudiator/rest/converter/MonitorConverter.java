46dccfe04f82d0d2e89b1cfe2377f55c571a8806
Volker Foth <volker.foth@uni-ulm.de>
2018-06-05 12:33:27 +0200

    Converterimplementation

--- src/main/java/io/github/cloudiator/rest/converter/MonitorConverter.java
+++ src/main/java/io/github/cloudiator/rest/converter/MonitorConverter.java
@@ -8,10 +8,23 @@ import org.cloudiator.messages.entities.MonitorEntities;
  */
 public class MonitorConverter implements TwoWayConverter<Monitor, MonitorEntities.Monitor> {
 
+  private final MonitorTargetConverter monitorTargetConverter = new MonitorTargetConverter();
+  private final SensorConverter sensorConverter = new SensorConverter();
+  private final MonitorTagConverter monitorTagConverter = new MonitorTagConverter();
+  private final DataSinkConverter sinkConverter = new DataSinkConverter();
 
   @Override
   public Monitor applyBack(MonitorEntities.Monitor kafkamonitor) {
-    Monitor restmonitor = new Monitor();
+    Monitor restmonitor = new Monitor()
+        .metric(kafkamonitor.getMetric())
+        .sensor(sensorConverter.applyBack(kafkamonitor.getSensor()));
+
+    if (!kafkamonitor.getTargetList().isEmpty()) {
+      for (MonitorEntities.MonitoringTarget monTag : kafkamonitor.getTargetList()) {
+        restmonitor.addTargetsItem(monitorTargetConverter.applyBack(monTag));
+      }
+    }
+   
     return null;
   }
 
