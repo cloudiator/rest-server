4406f4c2110977cca3c21b6d4526d06180fe8e0d
Daniel Baur <daniel.baur@uni-ulm.de>
2018-06-25 12:07:20 +0200

    Correct handling of token handling

--- src/main/java/io/swagger/configuration/ApiKeyAuthorizationFilter.java
+++ src/main/java/io/swagger/configuration/ApiKeyAuthorizationFilter.java
@@ -3,23 +3,17 @@ package io.swagger.configuration;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static io.swagger.configuration.SecurityConstants.HEADER;
 
-import de.uniulm.omi.cloudiator.util.configuration.Configuration;
-import io.github.cloudiator.rest.api.ApiException;
-import io.github.cloudiator.rest.model.Token;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.annotation.Nullable;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.cloudiator.messages.entities.User.AuthRequest;
 import org.cloudiator.messages.entities.User.AuthResponse;
-import org.cloudiator.messages.entities.UserEntities;
 import org.cloudiator.messaging.ResponseException;
 import org.cloudiator.messaging.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -43,66 +37,30 @@ public class ApiKeyAuthorizationFilter extends BasicAuthenticationFilter {
 
     String header = request.getHeader(HEADER);
 
-    //|| !checkToken(header)
-    if (header == null) {
-      chain.doFilter(request, response);
-      return;
+    if (header != null) {
+      final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = checkToken(
+          header);
+      if (usernamePasswordAuthenticationToken != null) {
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+      }
     }
 
-    SecurityContextHolder.getContext().setAuthentication(getToken(request));
     chain.doFilter(request, response);
 
   }
 
 
-  private boolean checkToken(String token) {
-    final String validToken = Configuration.conf().getString(SecurityConstants.TOKEN_CONFIG);
-
-    if (validToken == null) {
-      return false;
-    }
-
-    return validToken.equals(token);
-  }
-
-  private UsernamePasswordAuthenticationToken getToken(HttpServletRequest httpServletRequest) {
-
-    String token = httpServletRequest.getHeader(HEADER);
-    System.out.println(token + "\n");
+  @Nullable
+  private UsernamePasswordAuthenticationToken checkToken(String handedToken) {
 
     try {
-      if (token == null) {
-        throw new ApiException(500, "tokerError");
-      }
-
-      UserEntities.Token kafkaToken = UserEntities.Token.newBuilder().setToken(token)
-          .setGenerationTime(1).setExpireTime(2).build();
-      AuthRequest req = AuthRequest.newBuilder().setToken(kafkaToken).build();
-      AuthResponse authResponse;
-      authResponse = userService.auth(req);
-      //System.out.println(authResponse + "\n");
+      final AuthRequest req = AuthRequest.newBuilder().setHandedToken(handedToken).build();
+      final AuthResponse authResponse = userService.auth(req);
       String user = authResponse.getUser().getEmail();
       String userTenant = authResponse.getUser().getTenant().getTenant();
-
       return new UsernamePasswordAuthenticationToken(user, userTenant, new ArrayList<>());
-    } catch (ResponseException ex) {
-      throw new ApiException(ex.code(), ex.getMessage() + token);
+    } catch (ResponseException e) {
+      return null;
     }
-
   }
- /*
-  private boolean checkToken(String token) {
-    final String validToken = Configuration.conf().getString(SecurityConstants.TOKEN_CONFIG);
-    if (validToken == null) {
-      return false;
-    }
-    return validToken.equals(token);
-  }
-
-
-  private UsernamePasswordAuthenticationToken getToken(HttpServletRequest httpServletRequest) {
-    return new UsernamePasswordAuthenticationToken("dummy_user_id", null, new ArrayList<>());
-  }
-*/
-
 }
--- src/main/java/io/swagger/configuration/SecurityConstants.java
+++ src/main/java/io/swagger/configuration/SecurityConstants.java
@@ -3,8 +3,5 @@ package io.swagger.configuration;
 public class SecurityConstants {
 
   static final String HEADER = "X-API-Key";
-  static final String TOKEN_CONFIG = "security.apiKey";
-  static final long EXPIRE_TIME = 604800000; // 7 Tage
-  public static final String HEADER_STRING = "Authorization";
-
+  
 }
