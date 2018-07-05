fd6afdbd4e6cc6a0ee80e1b0cc3372f378f0698b
Daniel Baur <daniel.baur@uni-ulm.de>
2018-07-05 14:43:48 +0200

    Fixed beans.xml

--- src/main/java/io/github/cloudiator/rest/api/DecryptionApiController.java
+++ src/main/java/io/github/cloudiator/rest/api/DecryptionApiController.java
@@ -1,7 +1,9 @@
 package io.github.cloudiator.rest.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
+import io.swagger.annotations.ApiParam;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
 import org.cloudiator.messaging.services.EncryptionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,52 +12,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.constraints.*;
-import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
 
 @Controller
 public class DecryptionApiController implements DecryptionApi {
 
-    private static final Logger log = LoggerFactory.getLogger(DecryptionApiController.class);
+  private static final Logger log = LoggerFactory.getLogger(DecryptionApiController.class);
 
-    private final ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
 
-    private final HttpServletRequest request;
+  private final HttpServletRequest request;
 
-    @Autowired
-    private EncryptionService encryptionService;
+  @Autowired
+  private EncryptionService encryptionService;
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public DecryptionApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
+  @org.springframework.beans.factory.annotation.Autowired
+  public DecryptionApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+    this.objectMapper = objectMapper;
+    this.request = request;
+  }
+
+  public ResponseEntity<String> decrypt(
+      @ApiParam(value = "Text to decrypt", required = true) @PathVariable("text") String text) {
+
+    String accept = request.getHeader("Accept");
+    if (accept != null && accept.contains("application/json")) {
+      try {
+        return new ResponseEntity<String>(objectMapper.readValue("\"\"", String.class),
+            HttpStatus.NOT_IMPLEMENTED);
+      } catch (IOException e) {
+        log.error("Couldn't serialize response for content type application/json", e);
+        return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+      }
     }
 
-    public ResponseEntity<String> decrypt(@ApiParam(value = "Text to decrypt",required=true) @PathVariable("text") String text) {
-
-
-
-
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<String>(objectMapper.readValue("\"\"", String.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<String>(HttpStatus.NOT_IMPLEMENTED);
-    }
+    return new ResponseEntity<String>(HttpStatus.NOT_IMPLEMENTED);
+  }
 
 }
