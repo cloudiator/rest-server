a32b90f4e06c5fbf93f058c0ff260b632c2d3c7e
Daniel Baur <daniel.baur@uni-ulm.de>
2018-10-16 15:02:27 +0200

    Added process converter

--- src/main/java/io/github/cloudiator/rest/converter/ScheduleConverter.java
+++ src/main/java/io/github/cloudiator/rest/converter/ScheduleConverter.java
@@ -4,8 +4,11 @@ import de.uniulm.omi.cloudiator.util.OneWayConverter;
 import io.github.cloudiator.rest.model.Schedule;
 import io.github.cloudiator.rest.model.Schedule.InstantiationEnum;
 import org.cloudiator.messages.entities.ProcessEntities;
+import org.cloudiator.messages.entities.ProcessEntities.Process;
 
 public class ScheduleConverter implements OneWayConverter<ProcessEntities.Schedule, Schedule> {
+
+  private static final ProcessConverter PROCESS_CONVERTER = ProcessConverter.INSTANCE;
 
   @Override
   public Schedule apply(ProcessEntities.Schedule schedule) {
@@ -14,6 +17,10 @@ public class ScheduleConverter implements OneWayConverter<ProcessEntities.Schedu
     result.setId(schedule.getId());
     result.setInstantiation(InstantiationEnum.fromValue(schedule.getInstantiation().name()));
     result.setJob(schedule.getJob());
+
+    for (Process process : schedule.getProcessesList()) {
+      result.addProcessesItem(PROCESS_CONVERTER.applyBack(process));
+    }
 
     return result;
   }
