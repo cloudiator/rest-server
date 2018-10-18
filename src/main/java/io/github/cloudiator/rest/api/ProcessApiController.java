414f54d295f450b1bb8e9f8e460b5fb5820cd96c
Daniel Baur <daniel.baur@uni-ulm.de>
2018-10-18 16:00:04 +0200

    Fixed annotations

--- src/main/java/io/github/cloudiator/rest/api/ProcessApiController.java
+++ src/main/java/io/github/cloudiator/rest/api/ProcessApiController.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -79,7 +80,8 @@ public class ProcessApiController implements ProcessApi {
   }
 
   @Override
-  public ResponseEntity<Process> findProcess(String id) {
+  public ResponseEntity<Process> findProcess(
+      @ApiParam(value = "Unique identifier of the resource", required = true) @PathVariable("id") String id) {
     String accept = request.getHeader("Accept");
     if (accept != null && accept.contains("application/json")) {
 
