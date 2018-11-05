9818c032392325c62f417f7265069186c045fb58
Daniel Baur <daniel.baur@uni-ulm.de>
2018-11-05 17:27:14 +0100

    Fixed node id encoding in processes

--- server/src/main/java/io/github/cloudiator/rest/api/ProcessApiController.java
+++ server/src/main/java/io/github/cloudiator/rest/api/ProcessApiController.java
@@ -1,6 +1,7 @@
 package io.github.cloudiator.rest.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Function;
 import com.google.common.base.Strings;
 import io.github.cloudiator.rest.UserInfo;
 import io.github.cloudiator.rest.converter.ProcessConverter;
@@ -10,6 +11,8 @@ import io.github.cloudiator.rest.model.ProcessNew;
 import io.github.cloudiator.rest.model.Queue;
 import io.github.cloudiator.rest.queue.QueueService;
 import io.github.cloudiator.rest.queue.QueueService.QueueItem;
+import io.github.cloudiator.util.Base64IdEncoder;
+import io.github.cloudiator.util.IdEncoder;
 import io.swagger.annotations.ApiParam;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,6 +45,7 @@ public class ProcessApiController implements ProcessApi {
 
   private static final ProcessNewConverter PROCESS_NEW_CONVERTER = ProcessNewConverter.INSTANCE;
   private static final ProcessConverter PROCESS_CONVERTER = ProcessConverter.INSTANCE;
+  private final IdEncoder idEncoder = Base64IdEncoder.create();
 
   private final ProcessService processService;
 
@@ -62,6 +66,8 @@ public class ProcessApiController implements ProcessApi {
     if (accept != null && accept.contains("application/json")) {
 
       final String tenant = UserInfo.of(request).tenant();
+
+      process.setNode(idEncoder.encode(process.getNode()));
 
       final CreateProcessRequest createProcessRequest = CreateProcessRequest.newBuilder()
           .setUserId(tenant).setProcess(PROCESS_NEW_CONVERTER.apply(process)).build();
@@ -113,8 +119,11 @@ public class ProcessApiController implements ProcessApi {
           throw new ApiException(500, "Multiple process found for id");
         }
 
-        return new ResponseEntity<>(
-            PROCESS_CONVERTER.applyBack(processQueryResponse.getProcesses(0)), HttpStatus.OK);
+        final Process process = PROCESS_CONVERTER.applyBack(processQueryResponse.getProcesses(0));
+        process.setNode(idEncoder.encode(process.getNode()));
+
+        return new ResponseEntity<>(process
+            , HttpStatus.OK);
 
 
       } catch (ResponseException e) {
@@ -143,7 +152,8 @@ public class ProcessApiController implements ProcessApi {
             .queryProcesses(builder.build());
 
         return new ResponseEntity<>(
-            processQueryResponse.getProcessesList().stream().map(PROCESS_CONVERTER::applyBack)
+            processQueryResponse.getProcessesList().stream().map(PROCESS_CONVERTER::applyBack).map(
+                (Function<Process, Process>) input -> input.node(idEncoder.encode(input.getNode())))
                 .collect(
                     Collectors.toList()), HttpStatus.OK);
 
