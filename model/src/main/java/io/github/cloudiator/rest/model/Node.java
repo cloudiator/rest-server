a5ba3fe33470e7284e1104407ae91bd8d30f2f47
Daniel Baur <daniel.baur@uni-ulm.de>
2019-02-25 15:24:58 +0100

    fixed node converter

--- model/src/main/java/io/github/cloudiator/rest/model/Node.java
+++ model/src/main/java/io/github/cloudiator/rest/model/Node.java
@@ -37,9 +37,7 @@ public class Node   {
    * The state the node is currently in. 
    */
   public enum StateEnum {
-    CREATED("CREATED"),
-    
-    FAILED("FAILED"),
+    PENDING("PENDING"),
     
     RUNNING("RUNNING"),
     
